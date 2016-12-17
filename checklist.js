b71216467b594ace5629dcbbee81edab799465d0
leesandra <leesandra.alexandre@gmail.com>
2016-12-17 13:49:45 -0500

    tracker updates

--- checklist.js
+++ checklist.js
@@ -1,19 +1,40 @@
 $(document).ready(function(){
-	console.log("ready")
+$("checkbox").on("click", function(){
+   console.log("click")
+  var toAdd = $("#input").val();
+  $("#items").append("<li>" + toAdd + "</li>");
+
+    })
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+//$(document).ready(function(){
+//	console.log("ready")
 	// Get a reference to the database service
-	var database = firebase.database();
-	function getUserData() {
-    	firebase.database().ref().once("value")
-   		.then(function(snapshot){
-  			console.log(snapshot.val()) 
-  			var results = snapshot.val().toDo
-  			var toDos = "<li></li>";
-  			_.forEach(results, function(result){
-  				console.log(result)
-  				toDos += "<li>" + result.text + "</li>"
-  			})
-  			$("#toDoList").append(toDos)
-  		})
-	}
-	getUserData()
-}) 
+//	var database = firebase.database();
+//	function getUserData() {
+ //   	firebase.database().ref().once("value")
+ //  		.then(function(snapshot){
+ // 			console.log(snapshot.val()) 
+ // 			var results = snapshot.val().toDo
+ // 			var toDos = "<li></li>";
+ // 			_.forEach(results, function(result){
+ // 				console.log(result)
+ // 				toDos += "<li>" + result.text + "</li>"
+ // 			})
+ // 			$("#toDoList").append(toDos)
+ // 		})
+ // }
+//	getUserData()
+//  }) 
